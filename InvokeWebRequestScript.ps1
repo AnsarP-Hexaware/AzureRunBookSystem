856cdff16d38fc137c866aceddb7a7c27530559b
AnsarP-Hexaware <66728111+AnsarP-Hexaware@users.noreply.github.com>
2020-07-14 17:36:54 +0530

    Update InvokeWebRequestScript.ps1
    Upgraded

--- InvokeWebRequestScript.ps1
+++ InvokeWebRequestScript.ps1
@@ -1,20 +1,43 @@
  
 Param
 (
-    [string]$Username,
-    [string]$Password,
-	[string]$Uri,
-	[string]$RequestItemSysId,
-	[string]$MethodType,
-	[string]$Body
-	
+    [string]$RequestItemSysId,
+    [string]$jobName,
+	[bool] $jobStatus,
+    [string]$exceptionMessage
 )
 
+try
+{
 
-
- $creds = [System.Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes($Username+':'+$Password))
+"Recevied REquest id is $RequestItemSysId"
+ $creds = "c3lzdGVtLmF1dG9tYXRpb24uaGV4YUBoZXhhLm1haWwuY29tOkhleGFBdXRvQDIwMjAu"
+ $Uri = "https://hexawaretechnologieshongkongltddemo5.service-now.com/api/now/table/sc_req_item"
+ $MethodType="PUT"
  $combinedUrl = $Uri+'/'+$RequestItemSysId
-  
- Invoke-WebRequest -Uri $combinedUrl -Method $MethodType -Headers @{ 'Authorization' = 'Basic ' +  $creds  }  -Body $Body
+
+
+if($jobStatus)
+{ 
+  $Body=  '{ "comments": "Request completed successfully",  "state": "3", "work_notes": "RunBook Executed Successfully"}'
+ }
+else
+{
+    $customObject = @{"work_notes" = $exceptionMessage; "state"= 2; "assignment_group" = "7f49d6581bbdd85029a0c95e0d4bcb26"}
+    $Body = ConvertTo-Json -InputObject $customObject
  
+}
+
+$Body
+Invoke-WebRequest -Uri $combinedUrl -UseBasicParsing -Method $MethodType -Headers @{ 'Authorization' = 'Basic ' +  $creds  }  -Body $Body
+
+
+}
+catch
+{
+     $ErrorMessage = $_.Exception.Message
+     $ErrorMessage
+     throw $ErrorMessage
+}
+
  
